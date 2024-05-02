099e1e7cd1b0578a07d36ed386d865a4d871ee59
Lijan Shrestha <lijanaayasta@gmail.com>
2024-05-02 19:57:02 +0545

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+
+}
