cdbe1878a33335ae40258cc4df3048b48773e358
Lijan Shrestha <lijanaayasta@gmail.com>
2024-05-02 20:34:51 +0545

    Raycast added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] float speed = 5f;
+    GameObject bulb;
     private Rigidbody2D body;
+    bool hasLOS;
 
     // Start is called before the first frame update
+    void Start()
+    {
+        bulb = GameObject.FindGameObjectWithTag("Bulb");
+    }
+    
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -20,6 +27,23 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, bulb.transform.position - transform.position);
+        if(ray.collider != null)
+        {
+            hasLOS = ray.collider.CompareTag("Bulb");
+            if(hasLOS)
+            {
+                Debug.DrawRay(transform.position, bulb.transform.position - transform.position, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, bulb.transform.position - transform.position, Color.red);
+            }
         }
     }
 
